18d697a4a851b408facf4e116b2a294d6ef913fc
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-06-06 12:58:28 +0300

    connecting MongoDB database

--- amazon_clone/server/index.js
+++ amazon_clone/server/index.js
@@ -1,7 +1,25 @@
-const express = require('express')
+//IMPORTS FROM PACKAGES
+const express = require('express');
+const mongoose = require('mongoose');
+
+//IMPORTS FROM OTHER FILES
+const authRouter = require('./routes/auth')
+
+//INIT
 const app = express();
 const PORT = 3000;
+const DB = "mongodb+srv://cipher:password22@cluster0.1okosds.mongodb.net/?retryWrites=true&w=majority";
 
+//middleware
+app.use(authRouter);
+
+
+//Connections
+mongoose.connect(DB).then(() => {
+  console.log('Connection Successful');
+}).catch(e => {
+  console.log(e);
+});
 
 app.listen(PORT,  () => {
   console.log(`Connected at port ${PORT}`);
