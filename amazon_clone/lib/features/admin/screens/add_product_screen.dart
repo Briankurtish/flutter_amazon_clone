06f774abd4b4e24fbe3c41b81b0d4283b7907057
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-06-07 15:07:52 +0300

    added dotted border widget

--- amazon_clone/lib/features/admin/screens/add_product_screen.dart
+++ amazon_clone/lib/features/admin/screens/add_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constants/global_variables.dart';
@@ -27,6 +28,45 @@ class _AddProductScreenState extends State<AddProductScreen> {
             style: TextStyle(color: Colors.black),
           ),
           centerTitle: true,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  dashPattern: const [10, 4],
+                  strokeCap: StrokeCap.round,
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.folder_open,
+                          size: 40,
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          'Select Product Images',
+                          style: TextStyle(color: Colors.grey.shade400),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
